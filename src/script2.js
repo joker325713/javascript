3a36a14df23e8edcf20a4c41a96af45d99d9c7c3
Marsel <shaidullov-marsel@mail.ru>
2024-04-20 23:16:39 +0300

    second step

---
+++ src/script2.js
@@ -0,0 +1,26 @@
+"use strict";
+///числа
+let number = 4.6;
+console.log(-4/0)
+console.log('string' * 9);
+
+///строчка
+const persone = 'ivan';
+
+const bool = true;
+
+console.log(something);
+
+///объекты
+const obj = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+//массивы 
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
---
+++ src/script3.js
@@ -0,0 +1,9 @@
+const arr = [1, 2, 3];
+console.log(arr[1]);
+
+const obj = {
+    Anna: 500,
+    Adelina: 900
+};
+
+console.log(obj.Adelina);
